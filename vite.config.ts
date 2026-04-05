339a6414b262fc0fb58b33a0285ffa68fb83c2c9
Akash <akash7501@gmail.com>
2026-04-06 00:40:49 +0530

    add bases root in module federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 import { federation } from "@module-federation/vite"
 
 export default defineConfig({
-  plugins: [react(),
+  base: "https://mfe-container-repo.s3.ap-south-1.amazonaws.com/cart/",
+
+  plugins: [
+    react(),
     federation({
       name: "cart",
       filename: "remoteEntry.js",
       exposes: {
-        "./Cart": "./src/assets/cart.tsx"
+        "./CartPage": "./src/CartPage.jsx"
       },
       shared: ["react", "react-dom"]
     })
-  ],
-  server: {
-    cors: true,
-    port: 3002,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  }
+  ]
 })
